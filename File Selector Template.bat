9e78e8f89ea9c3a429ac8f9cc5ae05666e03b59e
Jordan Lewis <jordantaylorlewis@gmail.com>
2025-12-01 16:37:01 -0500

    Update File Selector script off mshta and use a powershell implementation instead.

--- File Selector Template.bat
+++ File Selector Template.bat
@@ -1,10 +1,30 @@
+<# :
 @echo off
-set dialog="about:<input type=file id=FILE><script>FILE.click();new ActiveXObject('Scripting.FileSystemObject').GetStandardStream(1).WriteLine(FILE.value);close();resizeTo(0,0);</script>"
-for /f "tokens=* delims=" %%a in ('mshta.exe %dialog%') do set "fileName=%%~na" && set "fileExtension=%%~xa" && set "filePath=%%~da%%~pa"
-cls
+setlocal
+
+for /f "delims=" %%a in ('powershell -noprofile "iex (${%~f0} | out-string)"') do (
+    set "fileName=%%~na"
+    set "fileExtension=%%~xa"
+    set "filePath=%%~da%%~pa"
+)
+
+
 echo  Selected "%filePath%%fileName%%fileExtension%"
 echo.
 echo  Filename:         %fileName%
 echo  Extension:        %fileExtension:~1%
 echo  Path:             %filePath%
 pause
+
+goto :EOF
+
+: #>
+Add-Type -AssemblyName System.Windows.Forms
+$f = new-object Windows.Forms.OpenFileDialog
+$f.InitialDirectory = pwd
+$f.Filter = "All Files (*.*)|*.*"
+$f.ShowHelp = $true
+$f.Multiselect = $false
+[void]$f.ShowDialog()
+if ($f.Multiselect) { $f.FileNames } else { $f.FileName }
+
